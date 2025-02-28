7941f90cfcedfee4f7b363048c716020791346d8
stana-ethernal <stana.miric@ethernal.tech>
2025-02-28 20:27:49 +0100

    extract fns

--- src/handlers.rs
+++ src/handlers.rs
@@ -2,49 +2,28 @@ use crate::authorization::create_execute_messages_for_authorization;
 use crate::cli::{PoolInfo, ProgramAction};
 use crate::config::Config;
 use crate::helpers::{
-    approve_library, create_base_account, get_filtered_authorizations,
-    instantiate_astro_lper_library, instantiate_astro_withdraw_library,
-    instantiate_authorization_and_processor, instantiate_splitter_library,
-    transfer_accounts_ownership,
+    approve_library, build_deploy_subroutine, build_withdraw_subroutine,
+    create_and_execute_authorization, create_base_account, create_output_accounts,
+    get_filtered_authorizations, instantiate_and_approve_astroport_libraries,
+    instantiate_authorization_and_processor, instantiate_splitter_library, transfer_ownership,
 };
 use crate::wasm::execute_wasm_contract;
 use anyhow::Result;
-use cw_ownable;
-use valence_authorization_utils::{
-    authorization::{AuthorizationModeInfo, PermissionTypeInfo},
-    authorization_message::{Message, MessageDetails, MessageType, ParamRestriction},
-    builders::{AtomicFunctionBuilder, AtomicSubroutineBuilder, AuthorizationBuilder},
-};
-use valence_library_utils::LibraryAccountType;
 use valence_processor_utils;
 
-pub const DELIMITER: &str = "_";
-pub const DEPLOY: &str = "deploy";
-pub const WITHDRAW: &str = "withdraw";
-
 pub fn create_program(label_prefix: &String, pools: &Vec<PoolInfo>, config: &Config) -> Result<()> {
-    println!("Creating program with label {}", label_prefix);
+    println!("Creating program with label {} ...", label_prefix);
 
     // Deploy authorization and processor
     let (authorization_address, processor_address) =
         instantiate_authorization_and_processor(&config)?;
-    println!("Authorization Address: {}", authorization_address);
-    println!("Processor Address: {}", processor_address);
 
     // Create input account
     let input_account = create_base_account(config)?;
     println!("Input Account Address: {}", input_account);
 
-    // Create multiple output accounts for splitter, liquidity deployment and withdrawal
-    let mut split_output_accounts = Vec::new();
-    let mut liquidity_output_accounts = Vec::new();
-    let mut withdrawal_accounts = Vec::new();
-
-    for _ in 0..pools.len() {
-        split_output_accounts.push(create_base_account(config)?.to_string());
-        liquidity_output_accounts.push(create_base_account(config)?.to_string());
-        withdrawal_accounts.push(create_base_account(config)?.to_string());
-    }
+    let (split_output_accounts, liquidity_output_accounts, withdrawal_accounts) =
+        create_output_accounts(&config, &pools)?;
 
     // Instantiate splitter library
     let split_lib_address = instantiate_splitter_library(
@@ -62,172 +41,40 @@ pub fn create_program(label_prefix: &String, pools: &Vec<PoolInfo>, config: &Con
     }
 
     // Instantiate Astroport LPer and Astroport Withdrawal libraries and approve them per pool
-    let mut astroport_lper_lib_addresses = Vec::new();
-    let mut astroport_withdraw_lib_addresses = Vec::new();
-    for ((pool, split_output_account), liquidity_output_account) in pools
-        .iter()
-        .zip(&split_output_accounts)
-        .zip(&liquidity_output_accounts)
-    {
-        // deploy library
-        let astroport_lper_lib_address = instantiate_astro_lper_library(
-            config,
-            pool,
-            split_output_account,
-            liquidity_output_account,
+    let (astroport_lper_lib_addresses, astroport_withdraw_lib_addresses) =
+        instantiate_and_approve_astroport_libraries(
+            &config,
+            &pools,
+            &split_output_accounts,
+            &liquidity_output_accounts,
             &processor_address,
         )?;
-        approve_library(config, split_output_account, &astroport_lper_lib_address)?;
-        approve_library(
-            config,
-            liquidity_output_account,
-            &astroport_lper_lib_address,
-        )?;
-        astroport_lper_lib_addresses.push(astroport_lper_lib_address);
 
-        // withdraw library
-        let astroport_withdraw_lib_address = instantiate_astro_withdraw_library(
-            config,
-            pool,
-            liquidity_output_account,
-            &processor_address,
-        )?;
-        approve_library(
-            config,
-            liquidity_output_account,
-            &astroport_withdraw_lib_address,
-        )?;
-        astroport_withdraw_lib_addresses.push(astroport_withdraw_lib_address);
-    }
+    // Create deployment subroutines
+    let deploy_subroutine =
+        build_deploy_subroutine(&split_lib_address, &astroport_lper_lib_addresses);
+    let withdraw_subroutine = build_withdraw_subroutine(&astroport_withdraw_lib_addresses);
 
-    // Create deployment subroutine
-    let mut deploy_subroutine_builder = AtomicSubroutineBuilder::new();
-
-    // Add the splitter function
-    deploy_subroutine_builder = deploy_subroutine_builder.with_function(
-        AtomicFunctionBuilder::new()
-            .with_contract_address(LibraryAccountType::Addr(split_lib_address.to_string()))
-            .with_message_details(MessageDetails {
-                message_type: MessageType::CosmwasmExecuteMsg,
-                message: Message {
-                    name: "process_function".to_string(),
-                    params_restrictions: Some(vec![ParamRestriction::MustBeIncluded(vec![
-                        "process_function".to_string(),
-                        "split".to_string(),
-                    ])]),
-                },
-            })
-            .build(),
-    );
-
-    // Add all LPER library functions
-    for astroport_lper_lib_address in &astroport_lper_lib_addresses {
-        deploy_subroutine_builder = deploy_subroutine_builder.with_function(
-            AtomicFunctionBuilder::new()
-                .with_contract_address(LibraryAccountType::Addr(
-                    astroport_lper_lib_address.to_string(),
-                ))
-                .with_message_details(MessageDetails {
-                    message_type: MessageType::CosmwasmExecuteMsg,
-                    message: Message {
-                        name: "process_function".to_string(),
-                        params_restrictions: Some(vec![ParamRestriction::MustBeIncluded(vec![
-                            "process_function".to_string(),
-                            "provide_double_sided_liquidity".to_string(),
-                        ])]),
-                    },
-                })
-                .build(),
-        );
-    }
-
-    // Create a single authorization with all functions
-    let deploy_authorization = AuthorizationBuilder::new()
-        .with_label(&format!("{}{}{}", label_prefix, DELIMITER, DEPLOY))
-        .with_mode(AuthorizationModeInfo::Permissioned(
-            PermissionTypeInfo::WithoutCallLimit(vec![config.tool_operator_address.to_string()]),
-        ))
-        .with_subroutine(deploy_subroutine_builder.build())
-        .build();
-
-    // Create withdraw subroutine
-    let mut withdraw_subroutine_builder = AtomicSubroutineBuilder::new();
-
-    // Add all withdraw library functions
-    for astroport_withdraw_lib_address in &astroport_withdraw_lib_addresses {
-        withdraw_subroutine_builder = withdraw_subroutine_builder.with_function(
-            AtomicFunctionBuilder::new()
-                .with_contract_address(LibraryAccountType::Addr(
-                    astroport_withdraw_lib_address.to_string(),
-                ))
-                .with_message_details(MessageDetails {
-                    message_type: MessageType::CosmwasmExecuteMsg,
-                    message: Message {
-                        name: "process_function".to_string(),
-                        params_restrictions: Some(vec![ParamRestriction::MustBeIncluded(vec![
-                            "process_function".to_string(),
-                            "withdraw_liquidity".to_string(),
-                        ])]),
-                    },
-                })
-                .build(),
-        );
-    }
-
-    // Create a single withdraw authorization with all functions
-    let withdraw_authorization = AuthorizationBuilder::new()
-        .with_label(&format!("{}{}{}", label_prefix, DELIMITER, WITHDRAW))
-        .with_mode(AuthorizationModeInfo::Permissioned(
-            PermissionTypeInfo::WithoutCallLimit(vec![config.tool_operator_address.to_string()]),
-        ))
-        .with_subroutine(withdraw_subroutine_builder.build())
-        .build();
-
-    let create_authorization_msg = valence_authorization_utils::msg::ExecuteMsg::PermissionedAction(
-        valence_authorization_utils::msg::PermissionedMsg::CreateAuthorizations {
-            authorizations: vec![deploy_authorization, withdraw_authorization],
-        },
-    );
-
-    // Create authorizations
-    execute_wasm_contract(
+    // Create Authorization Messages and Execute
+    create_and_execute_authorization(
         &authorization_address,
-        &serde_json::to_string(&create_authorization_msg)?,
-        config,
-    )?;
-
-    // Transfer ownership of authorization contract
-    let update_auth_ownership_msg = valence_authorization_utils::msg::ExecuteMsg::UpdateOwnership(
-        cw_ownable::Action::TransferOwnership {
-            new_owner: config.neutron_dao_committee_address.to_string(),
-            expiry: None,
-        },
-    );
-
-    execute_wasm_contract(
-        &authorization_address,
-        &serde_json::to_string(&update_auth_ownership_msg)?,
-        config,
-    )?;
-
-    // Transfer ownership of the valence accounts
-    let mut all_accounts = Vec::new();
-    all_accounts.push(input_account.clone());
-    all_accounts.extend_from_slice(&split_output_accounts);
-    all_accounts.extend_from_slice(&liquidity_output_accounts);
-    all_accounts.extend_from_slice(&withdrawal_accounts);
-
-    transfer_accounts_ownership(
+        deploy_subroutine,
+        withdraw_subroutine,
         &config,
-        &all_accounts,
-        &config.neutron_dao_committee_address,
+        label_prefix,
     )?;
 
-    println!("Ownership change needs to be accepted for the following accounts:");
-    for acc in &all_accounts {
-        println!("{}", acc);
-    }
+    // Transfer Ownership Athorization Contract and of Valence Accounts
+    transfer_ownership(
+        &config,
+        &authorization_address,
+        &input_account,
+        &split_output_accounts,
+        &liquidity_output_accounts,
+        &withdrawal_accounts,
+    )?;
 
+    println!("Deployment completed successfully!");
     Ok(())
 }
 
@@ -236,6 +83,10 @@ pub fn execute_program(
     action: ProgramAction,
     config: &Config,
 ) -> Result<()> {
+    println!(
+        "Executing program for contract {} ...",
+        auth_contract_address
+    );
     // Get the filtered authorizations based on action type
     let authorizations = get_filtered_authorizations(auth_contract_address, action, config)?;
 
@@ -264,6 +115,10 @@ pub fn execute_program(
 }
 
 pub fn tick_processor(processor_contract_address: &String, config: &Config) -> Result<()> {
+    println!(
+        "Ticking the processor on address {} ...",
+        processor_contract_address
+    );
     let tick_msg = valence_processor_utils::msg::ExecuteMsg::PermissionlessAction(
         valence_processor_utils::msg::PermissionlessMsg::Tick {},
     );
--- src/helpers.rs
+++ src/helpers.rs
@@ -1,7 +1,6 @@
 use crate::authorization::Authorization;
 use crate::cli::{PoolInfo, ProgramAction};
 use crate::config::Config;
-use crate::handlers::{DEPLOY, WITHDRAW};
 use crate::wasm::{
     execute_wasm_contract, get_authorizations, get_code_hash, instantiate2_wasm_contract,
     instantiate_wasm_contract,
@@ -10,6 +9,7 @@ use anyhow::{Error, Result};
 use bech32::{encode, primitives::decode::CheckedHrpstring, Bech32, Hrp};
 use chrono::Utc;
 use cosmwasm_std::{instantiate2_address, Addr, CanonicalAddr, HexBinary};
+use cw_ownable;
 use serde_json::Value;
 use std::collections::HashMap;
 use valence_account_utils::msg::{ExecuteMsg, InstantiateMsg};
@@ -17,12 +17,20 @@ use valence_astroport_lper::msg::{
     LibraryConfig as AstroLperLibraryConfig, LiquidityProviderConfig,
 };
 use valence_astroport_withdrawer::msg::LibraryConfig as AstroWithdrawerLibraryConfig;
+use valence_authorization_utils::{
+    authorization::{AuthorizationModeInfo, PermissionTypeInfo, Subroutine},
+    authorization_message::{Message, MessageDetails, MessageType, ParamRestriction},
+    builders::{AtomicFunctionBuilder, AtomicSubroutineBuilder, AuthorizationBuilder},
+};
 use valence_library_utils::{
     denoms::UncheckedDenom, liquidity_utils::AssetData, LibraryAccountType,
 };
 use valence_splitter_library::msg::LibraryConfig as SpliterLibraryConfig;
 
 const NEUTRON_BECH32_PREFIX: &str = "neutron";
+const DELIMITER: &str = "_";
+const DEPLOY: &str = "deploy";
+const WITHDRAW: &str = "withdraw";
 
 pub fn create_base_account(config: &Config) -> Result<String> {
     let acc_instantiate_msg = InstantiateMsg {
@@ -38,6 +46,27 @@ pub fn create_base_account(config: &Config) -> Result<String> {
     )?;
 
     Ok(contract_address)
+}
+
+pub fn create_output_accounts(
+    config: &Config,
+    pools: &Vec<PoolInfo>,
+) -> Result<(Vec<String>, Vec<String>, Vec<String>)> {
+    let mut split_output_accounts = Vec::new();
+    let mut liquidity_output_accounts = Vec::new();
+    let mut withdrawal_accounts = Vec::new();
+
+    for _ in 0..pools.len() {
+        split_output_accounts.push(create_base_account(config)?.to_string());
+        liquidity_output_accounts.push(create_base_account(config)?.to_string());
+        withdrawal_accounts.push(create_base_account(config)?.to_string());
+    }
+
+    Ok((
+        split_output_accounts,
+        liquidity_output_accounts,
+        withdrawal_accounts,
+    ))
 }
 
 pub fn instantiate_splitter_library(
@@ -89,7 +118,57 @@ pub fn instantiate_splitter_library(
     Ok(contract_address)
 }
 
-pub fn instantiate_astro_lper_library(
+pub fn instantiate_and_approve_astroport_libraries(
+    config: &Config,
+    pools: &Vec<PoolInfo>,
+    split_output_accounts: &Vec<String>,
+    liquidity_output_accounts: &Vec<String>,
+    processor_address: &String,
+) -> Result<(Vec<String>, Vec<String>)> {
+    let mut astroport_lper_lib_addresses = Vec::new();
+    let mut astroport_withdraw_lib_addresses = Vec::new();
+
+    for ((pool, split_output_account), liquidity_output_account) in pools
+        .iter()
+        .zip(split_output_accounts)
+        .zip(liquidity_output_accounts)
+    {
+        let astroport_lper_lib_address = instantiate_astro_lper_library(
+            config,
+            pool,
+            split_output_account,
+            liquidity_output_account,
+            processor_address,
+        )?;
+        approve_library(config, split_output_account, &astroport_lper_lib_address)?;
+        approve_library(
+            config,
+            liquidity_output_account,
+            &astroport_lper_lib_address,
+        )?;
+        astroport_lper_lib_addresses.push(astroport_lper_lib_address);
+
+        let astroport_withdraw_lib_address = instantiate_astro_withdraw_library(
+            config,
+            pool,
+            liquidity_output_account,
+            processor_address,
+        )?;
+        approve_library(
+            config,
+            liquidity_output_account,
+            &astroport_withdraw_lib_address,
+        )?;
+        astroport_withdraw_lib_addresses.push(astroport_withdraw_lib_address);
+    }
+
+    Ok((
+        astroport_lper_lib_addresses,
+        astroport_withdraw_lib_addresses,
+    ))
+}
+
+fn instantiate_astro_lper_library(
     config: &Config,
     pool: &PoolInfo,
     input_addr: &String,
@@ -126,7 +205,7 @@ pub fn instantiate_astro_lper_library(
     Ok(contract_address)
 }
 
-pub fn instantiate_astro_withdraw_library(
+fn instantiate_astro_withdraw_library(
     config: &Config,
     pool: &PoolInfo,
     input_addr: &String,
@@ -200,6 +279,9 @@ pub fn instantiate_authorization_and_processor(config: &Config) -> Result<(Strin
         &authorization_salt,
     )?;
 
+    println!("Authorization Address: {}", authorization_address);
+    println!("Processor Address: {}", processor_address);
+
     Ok((authorization_address, processor_address))
 }
 
@@ -264,6 +346,154 @@ pub fn get_filtered_authorizations(
         .collect();
 
     Ok(filtered_authorizations)
+}
+
+pub fn build_deploy_subroutine(
+    split_lib_address: &String,
+    astroport_lper_lib_addresses: &Vec<String>,
+) -> Subroutine {
+    let mut deploy_subroutine_builder = AtomicSubroutineBuilder::new();
+
+    deploy_subroutine_builder = deploy_subroutine_builder.with_function(
+        AtomicFunctionBuilder::new()
+            .with_contract_address(LibraryAccountType::Addr(split_lib_address.to_string()))
+            .with_message_details(MessageDetails {
+                message_type: MessageType::CosmwasmExecuteMsg,
+                message: Message {
+                    name: "process_function".to_string(),
+                    params_restrictions: Some(vec![ParamRestriction::MustBeIncluded(vec![
+                        "process_function".to_string(),
+                        "split".to_string(),
+                    ])]),
+                },
+            })
+            .build(),
+    );
+
+    for astroport_lper_lib_address in astroport_lper_lib_addresses {
+        deploy_subroutine_builder = deploy_subroutine_builder.with_function(
+            AtomicFunctionBuilder::new()
+                .with_contract_address(LibraryAccountType::Addr(
+                    astroport_lper_lib_address.to_string(),
+                ))
+                .with_message_details(MessageDetails {
+                    message_type: MessageType::CosmwasmExecuteMsg,
+                    message: Message {
+                        name: "process_function".to_string(),
+                        params_restrictions: Some(vec![ParamRestriction::MustBeIncluded(vec![
+                            "process_function".to_string(),
+                            "provide_double_sided_liquidity".to_string(),
+                        ])]),
+                    },
+                })
+                .build(),
+        );
+    }
+
+    deploy_subroutine_builder.build()
+}
+
+pub fn build_withdraw_subroutine(astroport_withdraw_lib_addresses: &Vec<String>) -> Subroutine {
+    let mut withdraw_subroutine_builder = AtomicSubroutineBuilder::new();
+
+    for astroport_withdraw_lib_address in astroport_withdraw_lib_addresses {
+        withdraw_subroutine_builder = withdraw_subroutine_builder.with_function(
+            AtomicFunctionBuilder::new()
+                .with_contract_address(LibraryAccountType::Addr(
+                    astroport_withdraw_lib_address.to_string(),
+                ))
+                .with_message_details(MessageDetails {
+                    message_type: MessageType::CosmwasmExecuteMsg,
+                    message: Message {
+                        name: "process_function".to_string(),
+                        params_restrictions: Some(vec![ParamRestriction::MustBeIncluded(vec![
+                            "process_function".to_string(),
+                            "withdraw_liquidity".to_string(),
+                        ])]),
+                    },
+                })
+                .build(),
+        );
+    }
+
+    withdraw_subroutine_builder.build()
+}
+
+pub fn create_and_execute_authorization(
+    authorization_address: &String,
+    deploy_subroutine: Subroutine,
+    withdraw_subroutine: Subroutine,
+    config: &Config,
+    label_prefix: &String,
+) -> Result<()> {
+    let deploy_authorization = AuthorizationBuilder::new()
+        .with_label(&format!("{}{}{}", label_prefix, DELIMITER, DEPLOY))
+        .with_mode(AuthorizationModeInfo::Permissioned(
+            PermissionTypeInfo::WithoutCallLimit(vec![config.tool_operator_address.to_string()]),
+        ))
+        .with_subroutine(deploy_subroutine)
+        .build();
+
+    let withdraw_authorization = AuthorizationBuilder::new()
+        .with_label(&format!("{}{}{}", label_prefix, DELIMITER, WITHDRAW))
+        .with_mode(AuthorizationModeInfo::Permissioned(
+            PermissionTypeInfo::WithoutCallLimit(vec![config.tool_operator_address.to_string()]),
+        ))
+        .with_subroutine(withdraw_subroutine)
+        .build();
+
+    let create_authorization_msg = valence_authorization_utils::msg::ExecuteMsg::PermissionedAction(
+        valence_authorization_utils::msg::PermissionedMsg::CreateAuthorizations {
+            authorizations: vec![deploy_authorization, withdraw_authorization],
+        },
+    );
+
+    execute_wasm_contract(
+        authorization_address,
+        &serde_json::to_string(&create_authorization_msg)?,
+        config,
+    )?;
+
+    Ok(())
+}
+
+pub fn transfer_ownership(
+    config: &Config,
+    authorization_address: &String,
+    input_account: &String,
+    split_output_accounts: &Vec<String>,
+    liquidity_output_accounts: &Vec<String>,
+    withdrawal_accounts: &Vec<String>,
+) -> Result<()> {
+    // Transfer ownership of the authorization contract
+    let update_auth_ownership_msg = valence_authorization_utils::msg::ExecuteMsg::UpdateOwnership(
+        cw_ownable::Action::TransferOwnership {
+            new_owner: config.neutron_dao_committee_address.to_string(),
+            expiry: None,
+        },
+    );
+
+    execute_wasm_contract(
+        authorization_address,
+        &serde_json::to_string(&update_auth_ownership_msg)?,
+        config,
+    )?;
+
+    // Collect all accounts
+    let mut all_accounts = vec![input_account.clone()];
+    all_accounts.extend_from_slice(&split_output_accounts);
+    all_accounts.extend_from_slice(&liquidity_output_accounts);
+    all_accounts.extend_from_slice(&withdrawal_accounts);
+
+    // Transfer ownership of all accounts
+    transfer_accounts_ownership(config, &all_accounts, &config.neutron_dao_committee_address)?;
+
+    println!("Ownership change needs to be accepted for the following accounts:");
+    for acc in &all_accounts {
+        println!("{}", acc);
+    }
+
+    Ok(())
 }
 
 /// Computes the predicted contract address based on CosmWasm's derivation formula.
